7fed83a6795faaecf7251070aad0c9da77dcee5b
keidra <keidrabaldwin@gmail.com>
2016-04-07 22:22:07 -0700

    complete word scramble game

--- game/script.js
+++ game/script.js
@@ -1,12 +1,7 @@
-
-
-
-String.prototype.shuffle = function () {
-    
+String.prototype.shuffle = function() {
     var a = this.split(""),
         n = a.length;
-
-    for(var i = n - 1; i > 0; i--) {
+    for (var i = n - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
         var tmp = a[i];
         a[i] = a[j];
@@ -15,109 +10,68 @@ String.prototype.shuffle = function () {
     return a.join("");
 }
 
-
 function getRandomInt(min, max) {
-   return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-var wordbank = ['website', 'virtual', 'version', 'utility', 'toolbar', 'storage', 'spyware', 'spammer', 'scanner', 'runtime', 'restore', 
-'program', 'process', 'privacy', 'printer', 'podcast', 'offline', 'network', 'monitor', 'malware', 'lurking', 'keyword', 
-'integer', 'exabyte', 'encrypt', 'dynamic', 'digital', 'desktop', 'compile', 'command', 'captcha', 'browser'];
+var wordbank = ['website', 'virtual', 'version', 'utility', 'toolbar',
+    'storage', 'spyware', 'spammer', 'scanner', 'runtime', 'restore',
+    'program', 'process', 'privacy', 'printer', 'podcast', 'offline',
+    'network', 'monitor', 'malware', 'lurking', 'keyword', 'integer',
+    'exabyte', 'encrypt', 'dynamic', 'digital', 'desktop', 'compile',
+    'command', 'captcha', 'browser'
+];
 var rndNum = getRandomInt(0, wordbank.length - 1);
 var word = wordbank[rndNum];
 var currentWord = word;
-
+var score = 0;
 
 function play() {
-  console.log(word);
-  var rstring = word.shuffle();
-  document.getElementById("1").innerHTML = rstring.charAt(0);
-  document.getElementById("2").innerHTML = rstring.charAt(1);
-  document.getElementById("3").innerHTML = rstring.charAt(2);
-  document.getElementById("4").innerHTML = rstring.charAt(3);
-  document.getElementById("5").innerHTML = rstring.charAt(4);
-  document.getElementById("6").innerHTML = rstring.charAt(5);
-  document.getElementById("7").innerHTML = rstring.charAt(6);
+    console.log(word);
+    var rstring = word.shuffle();
+    document.getElementById("1").innerHTML = rstring.charAt(0);
+    document.getElementById("2").innerHTML = rstring.charAt(1);
+    document.getElementById("3").innerHTML = rstring.charAt(2);
+    document.getElementById("4").innerHTML = rstring.charAt(3);
+    document.getElementById("5").innerHTML = rstring.charAt(4);
+    document.getElementById("6").innerHTML = rstring.charAt(5);
+    document.getElementById("7").innerHTML = rstring.charAt(6);
 }
-
 var squares = document.getElementById("board").getElementsByTagName("td");
 for (var i in squares) {
-   squares[i].onclick = play;
+    squares[i].onclick = play;
 }
-
-document.getElementById("restart").onclick = function() {
+document.getElementById("next").onclick = function() {
     for (var i in squares) {
-       squares[i].innerHTML = '';
+        squares[i].innerHTML = '';
     }
     rndNum = getRandomInt(0, wordbank.length - 1);
     word = wordbank[rndNum];
-
 }
-
 document.getElementById("rules").onclick = function() {
-  swal(' Try to guess the scrambled word. Click on any tile to shuffle the letters. Good luck!');
+    swal(
+        ' Try to guess the scrambled word. Click on any tile to shuffle the letters. Good luck!'
+    );
 }
-
 document.getElementById("hint").onclick = function() {
-  swal("The first letter is " + word[0] + ".");
+    swal("The first letter is " + word[0] + ".");
 }
-
-
-$( '#board tr' ).sortable({
-  placeholder: '#board tr',
-  update: function checkOrder(event, ui) {
-
-
-    var tiles = $('td');
-    var tempWord = '';
-    for (var i = 0; i < tiles.length; i++) {
-      var letter = $(tiles[i]).text();
-      tempWord += letter;
+$('#board tr').sortable({
+    placeholder: '#board tr',
+    update: function checkOrder(event, ui) {
+        var tiles = $('td');
+        var tempWord = '';
+        for (var i = 0; i < tiles.length; i++) {
+            var letter = $(tiles[i]).text();
+            tempWord += letter;
+        }
+        if (tempWord === word) {
+            swal("Correct!", "You guessed the word!", "success");
+            $('ul').append('<li>' + word + '</li>');
+            score++
+            $('#score').text(score);
+        }
     }
-
-
-    if(tempWord === word) {
-      swal("Good job!", "You guessed the correct word!", "success");
-
-    
-
-
-
-    if (tempWord === word) {
-      $('ul').append('<li>' + word + '</li>');
-  
-}
-
-  }
-
-    }
-    // var score = parseInt(document.getElementById("score"));
-    // if (score == tempWord) {
-    //   console.log(score); 
-    // }
-
-})
-
-
-
-
-
-// var score = parseInt(document.getElementById("score").innerHTML);
-
-//     function scoreIncrement() {
-//     score++;
-//     document.getElementById('score').innerHTML = score;
-//     console.log(score);
-
-// }
-
-// $(document).on('click', function(event) {
-    
-
-
-// });
-
-
+});
 
 
 
